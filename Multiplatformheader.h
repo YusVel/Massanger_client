f1384d058366862879255a0a666c101036ef39eb
YusVel <yusupvelimetov@mail.ru>
2024-04-17 17:05:43 +0300

    123

---
+++ Multiplatformheader.h
@@ -0,0 +1,38 @@
+#if defined(_WIN32)
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winSock2.h>
+#include <ws2tcpip.h>
+#pragma comment (lib,"ws2_32.lib")
+
+#define ISVALIDSOCKET(s) ((s)!=INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define SOCKET int
+#define ISVALIDSOCKET(s) ((s)>=0)
+#define CLOSESOCKET(s) close(s)
+#define GETSOCKETERRNO() (errno)
+#endif
+
+#if !defined(IPV6_V6ONLY)
+#define IPV6_V6ONLY 27
+#endif
+
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define MSGSIZE 1024
--- client.cpp
+++ client.cpp
@@ -1,38 +1,4 @@
-#if defined(_WIN32)
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x600
-#endif
-#include <winSock2.h>
-#include <ws2tcpip.h>
-#pragma comment (lib,"ws2_32.lib")
-
-#define ISVALIDSOCKET(s) ((s)!=INVALID_SOCKET)
-#define GETSOCKETERRNO() (WSAGetLastError())
-#define CLOSESOCKET(s) closesocket(s)
-
-#else
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <errno.h>
-
-#define SOCKET int
-#define ISVALIDSOCKET(s) ((s)>=0)
-#define GETSOCKETERRNO() (errno)
-#define CLOSESOCKET(s) close(s)
-#endif
-
-#if !defined(IPV6_V6ONLY)
-#define IPV6_V6ONLY 27
-#endif
-
-#include <stdio.h>
-#include <string.h>
-#define MSGSIZE 1024
+#include "Multiplatformheader.h"
 
 int main()
 {
